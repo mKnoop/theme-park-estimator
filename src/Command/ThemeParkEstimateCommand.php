e39a40212820eb1bb2fd055ec0ad5b547f1a0dc0
Martin Knoop <martin.knoop@bestit-online.de>
2019-10-16 21:04:58 +0200

    Added simple ml example to demonstrate the library

--- src/Command/ThemeParkEstimateCommand.php
+++ src/Command/ThemeParkEstimateCommand.php
@@ -2,7 +2,9 @@
 
 namespace App\Command;
 
+use Phpml\ModelManager;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -12,10 +14,27 @@ class ThemeParkEstimateCommand extends Command
 
     protected function configure()
     {
-        $this->setDescription('Estimate the waiting time for a theme park');
+        $this
+            ->setDescription('Estimate the waiting time for a theme park')
+            ->addArgument('weekday', InputArgument::REQUIRED)
+            ->addArgument('weather', InputArgument::REQUIRED)
+            ->addArgument('vacation', InputArgument::REQUIRED);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        if (!file_exists(__DIR__ . '/../../data/model.dat')) {
+            $output->writeln('<error>Model not found. Did you do train it? (train commnad)</error>');
+            return;
+        }
+
+        $modelManager = new ModelManager();
+        $estimator = $modelManager->restoreFromFile(__DIR__ . '/../../data/model.dat');
+        $prediction = $estimator->predict([[
+            $input->getArgument('weekday'),
+            $input->getArgument('weather'),
+            $input->getArgument('vacation')
+        ]]);
+        $output->writeln(sprintf('Prediction is: <info>%s</info>', round($prediction[0], 2)));
     }
 }
